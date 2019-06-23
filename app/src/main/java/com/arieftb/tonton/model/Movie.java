4e1c0795170e2714ed3fea3da4aadc8548975904
arieftb <turbagus14@gmail.com>
2019-06-23 21:35:35 +0700

    [arieftb] - adding movie object class

---
+++ app/src/main/java/com/arieftb/tonton/model/Movie.java
@@ -0,0 +1,91 @@
+/*
+ * Developed by arieftb on 6/23/19 9:32 PM.
+ * Last Modified 6/23/19 9:32 PM.
+ * Copyright (c) 2019. All rights reserved.
+ * www.arieftb.com
+ */
+
+package com.arieftb.tonton.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
+    private String title, genre, description, releaseDate;
+    private Double rating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.title);
+        dest.writeString(this.genre);
+        dest.writeString(this.description);
+        dest.writeString(this.releaseDate);
+        dest.writeValue(this.rating);
+    }
+
+    private Movie(Parcel in) {
+        this.title = in.readString();
+        this.genre = in.readString();
+        this.description = in.readString();
+        this.releaseDate = in.readString();
+        this.rating = (Double) in.readValue(Double.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+}
