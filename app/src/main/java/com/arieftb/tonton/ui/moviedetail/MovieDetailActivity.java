78998655a513c74fe908a68791e1d553c474d987
arieftb <turbagus14@gmail.com>
2019-06-25 16:22:39 +0700

    [arieftb] - add movie detail

---
+++ app/src/main/java/com/arieftb/tonton/ui/moviedetail/MovieDetailActivity.java
@@ -0,0 +1,23 @@
+/*
+ * Developed by arieftb on 6/25/19 4:21 PM.
+ * Last Modified 6/25/19 4:21 PM.
+ * Copyright (c) 2019. All rights reserved.
+ * www.arieftb.com
+ */
+
+package com.arieftb.tonton.ui.moviedetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arieftb.tonton.R;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
