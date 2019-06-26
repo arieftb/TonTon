0cae13dcd8809d972d0babf0bb404180ce07735d
arieftb <turbagus14@gmail.com>
2019-06-26 11:17:25 +0700

    [arieftb]-adding layout tv show detail

---
+++ app/src/main/java/com/arieftb/tonton/ui/tvshowdetail/TvShowDetailActivity.java
@@ -0,0 +1,23 @@
+/*
+ * Developed by arieftb on 6/26/19 11:09 AM.
+ * Last Modified 6/26/19 11:09 AM.
+ * Copyright (c) 2019. All rights reserved.
+ * www.arieftb.com
+ */
+
+package com.arieftb.tonton.ui.tvshowdetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arieftb.tonton.R;
+
+public class TvShowDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv_show_detail);
+    }
+}
