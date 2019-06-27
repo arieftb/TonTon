4d4241b6f14fba61140829feb2ff27e5969af914
arieftb <turbagus14@gmail.com>
2019-06-28 06:28:20 +0700

    [arieftb] - adding unit test MoviesViewModel

---
+++ app/src/test/java/com/arieftb/tonton/ui/movie/MoviesViewModelTest.java
@@ -0,0 +1,34 @@
+/*
+ * Developed by arieftb on 6/28/19 6:23 AM.
+ * Last Modified 6/28/19 6:23 AM.
+ * Copyright (c) 2019. All rights reserved.
+ * www.arieftb.com
+ */
+
+package com.arieftb.tonton.ui.movie;
+
+import com.arieftb.tonton.model.Movie;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MoviesViewModelTest {
+
+    private MoviesViewModel viewModel;
+
+    @Before
+    public void setViewModel() {
+        viewModel = new MoviesViewModel();
+    }
+
+    @Test
+    public void getMovies() {
+        List<Movie> movies = viewModel.getMovies();
+        assertNotNull(movies);
+        assertEquals(18, movies.size());
+    }
+}
